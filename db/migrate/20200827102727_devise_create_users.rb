40686aed690cd32447d31a2c90a1ae35101b95cf
ChiekoTakatoku <chiekotakatoku@gmail.com>
2020-08-27 19:49:18 +0900

    テーブルの作成

--- db/migrate/20200827102727_devise_create_users.rb
+++ db/migrate/20200827102727_devise_create_users.rb
@@ -7,6 +7,14 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      ## 追加情報
+      t.string  :nickname,  null: false, default: ""
+      t.string  :first_name,  null: false, default: ""
+      t.string  :family_name,  null: false, default: ""
+      t.string  :first_name_kana, null: false, default: ""
+      t.string  :family_name_kana,  null: false, default: ""
+      t.date  :birthday,  null: false
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
