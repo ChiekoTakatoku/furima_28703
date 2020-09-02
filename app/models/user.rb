c9b68390aa3d2b1c50f3dda31a7d1a6ec93ab5f2
ChiekoTakatoku <chiekotakatoku@gmail.com>
2020-09-02 11:18:43 +0900

    Userモデル単体テストコード

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,19 @@
 class User < ApplicationRecord
-  has_many :products
-  has_many :purchase_info
-
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :products
+  has_many :purchase_info
+
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true#, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
+  validates :password_confirmation, presence: true
+  validates :birthday, presence: true
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+  validates :family_name_kana, presence: true, format: { with: /\A[ァ-ン]+\z/ }
+
 end
