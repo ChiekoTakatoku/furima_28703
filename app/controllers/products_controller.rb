6e7dcb121c310deb7deeba0baeb0cebf98a2aa1b
ChiekoTakatoku <chiekotakatoku@gmail.com>
2020-09-07 19:10:45 +0900

    product_controller.rbの修正2

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,11 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
     # @products = Product.order("created_at DESC")
   end
 
   def new
-    authenticate_user!
     @product = Product.new
   end
 
