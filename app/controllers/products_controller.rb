cb1b8dc8f8da9fc84628118aa50b52d0b96a634b
ChiekoTakatoku <chiekotakatoku@gmail.com>
2020-09-11 17:08:09 +0900

    Rubocopの実行

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,7 +3,7 @@ class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit]
 
   def index
-    @products = Product.order("created_at DESC")
+    @products = Product.order('created_at DESC')
   end
 
   def new
