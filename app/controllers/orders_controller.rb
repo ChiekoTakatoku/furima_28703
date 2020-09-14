f25f35328c04fce0ac2e21a591e9a8673e5d318b
ChiekoTakatoku <chiekotakatoku@gmail.com>
2020-09-14 12:40:48 +0900

    購入画面に商品情報の記載

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,12 @@
 class OrdersController < ApplicationController
+
+  def index
+    @product = Product.find_by(params[:id])
+  end
+
+  def create
+    # 複数のテーブルにデータを保存する処理を記入する
+    # saveの有無で遷移先を分岐させる
+  end
+
 end
